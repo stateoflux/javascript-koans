dda7d0bce8c858f49a3779ea628fee5c99fc5b7f
Wayne Montague <wayne.montague@gmail.com>
2013-09-18 12:49:48 -0700

    the adding of mulitples of 3 and 5 tests are passing

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -60,14 +60,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.chain(_.range(1000))
+      .filter(function(n) { return n % 3 === 0 || n % 5 === 0; })
+      .reduce(function(memo, x) { return memo + x; })
+      .value(); 
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
